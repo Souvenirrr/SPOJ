fa9be54f10bdfc789f5f27d670b9578dbe275225
saonholacloi <32154041+saonholacloi@users.noreply.github.com>
2019-07-12 15:23:03 +0700

    REPROAD SPOJ
    Done!!!

---
+++ REPROAD.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+int main(){
+	freopen("input.txt", "r", stdin);
+	int T;
+	cin>>T;
+	for(int tc = 1; tc <= T; tc++){
+		int n, k;
+		cin>>n>>k;
+		int a[10000];
+		for (int i = 0; i < n; i++)
+		{
+			cin>>a[i];
+		}
+		
+		int front = 0;
+		int max_len = 0;
+		int zeros = 0;
+
+		for(int i = 0; i < n; i++)
+		{
+			if(a[i] == 0){
+				zeros++;
+			}
+			if (zeros <= k)
+			{
+				max_len = max(i - front + 1, max_len);
+			}else{
+				for(front; front < i; front++){
+					if(a[front] == 0){
+						zeros--;
+					}
+					if(zeros < k){ 
+						front++;	
+						break;
+					}
+				 }
+			}
+		}
+		cout<<max_len<<endl;
+	}
+	
+	_getch();
+}
